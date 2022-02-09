28f66ae6d3b16f178062e47fb2da12c1105197a0
Suroo <SDHUGHES1998@GMAIL.COM>
2022-02-09 21:53:59 +0000

    added letter check when inputted

--- run.py
+++ run.py
@@ -6,7 +6,7 @@
 # Imports - random selection
 import random
 
-# Constants - 
+# Constants -
 # the below will show all the constants
 # the new constant will trigger when the
 # player answers a letter incorrectly
@@ -62,14 +62,15 @@ HANGMAN = ['''
       |
 =========''']
 
-# These will be the words used when 
+# These will be the words used when
 # it comes to the user guessing.
 # These words will be completely random when
 # the game begins // only one word will active
 
-WORDS = ["PYTHON", "MOMENT", "CODING", "MANSION", "TROUBLE", "SKAKE", "CODEINSTITUTE"]
+WORDS = ["PYTHON", "MOMENT", "CODING", "MANSION",
+         "TROUBLE", "SKAKE", "CODEINSTITUTE"]
 
-MAX_WORDS = len(HANGMAN) - 1
+max_WRONG = len(HANGMAN) - 1
 
 # Initialize Variables
 
@@ -83,9 +84,26 @@ current_guess = "-" * len(word)
 # this will count the action of all the incorrect attempts
 wrong_guesses = 0
 
-# Used Letter 
+# Used Letter
 used_letters = []
 
 # MLoop
 print("Welcome To Hangman")
-print("Go Try and Guess The Word")
+print("Go Try and Guess The Word to Win")
+
+while wrong_guesses < max_WRONG and current_guess != word:
+    print(HANGMAN[wrong_guesses])
+    print("You have used the following letters: ", used_letters)
+    print("Currently, the word is", current_guess)
+
+    guess = input("Enter your guess:")
+    guess = guess.upper()
+
+    # Letter Check (This will check the letters that the user has inputted already)
+    while guess in used_letters:
+      print("Sorry, you have already inputted this letter. Please choose another letter.", guess)
+      guess = input("Enter your guess")
+      guess = guess.upper()
+
+# Adds new guessed letters to a new list of guessed letters
+used_letters.append(guess)
